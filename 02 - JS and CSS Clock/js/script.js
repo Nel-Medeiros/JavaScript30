5a680425e05406fc4b17610b3f78d3e1257a3b2d
Nel-Medeiros <emanuelpaivamedeiros@gmail.com>
2020-06-16 12:14:35 -0300

    Day-Two-Ok JS-Clock

---
+++ 02 - JS and CSS Clock/js/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.sec-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hour / 12) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    console.log(hour, minutes, seconds);
+}
+
+setInterval(setDate, 1000);
